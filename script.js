74a8c76924c64388d57d5d738ff14e8f940a1eb8
DuckQck <130593369+DuckQck@users.noreply.github.com>
2023-08-08 11:17:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,42 +3,42 @@ var x=Math.floor(Math.random()*6)+1;
 console.log(n);
 switch(n){
     case 1:
-        document.querySelector("img.img1").src="images/dice1.png";
+        document.querySelector("img.img1").src="dice1.png";
         break;
     case 2:
-        document.querySelector("img.img1").src="images/dice2.png";
+        document.querySelector("img.img1").src="dice2.png";
         break;
     case 3:
-        document.querySelector("img.img1").src="images/dice3.png";
+        document.querySelector("img.img1").src="dice3.png";
         break;
     case 4:
-        document.querySelector("img.img1").src="images/dice4.png";
+        document.querySelector("img.img1").src="dice4.png";
         break;
     case 5:
-        document.querySelector("img.img1").src="images/dice5.png";
+        document.querySelector("img.img1").src="dice5.png";
         break;
     case 6:
-        document.querySelector("img.img1").src="images/dice6.png";
+        document.querySelector("img.img1").src="dice6.png";
         break;
         }
 switch(x){
         case 1:
-        document.querySelector("img.img2").src="images/dice1.png";
+        document.querySelector("img.img2").src="dice1.png";
         break;
     case 2:
-        document.querySelector("img.img2").src="images/dice2.png";
+        document.querySelector("img.img2").src="dice2.png";
         break;
     case 3:
-        document.querySelector("img.img2").src="images/dice3.png";
+        document.querySelector("img.img2").src="dice3.png";
         break;
     case 4:
-        document.querySelector("img.img2").src="images/dice4.png";
+        document.querySelector("img.img2").src="dice4.png";
         break;
     case 5:
-        document.querySelector("img.img2").src="images/dice5.png";
+        document.querySelector("img.img2").src="dice5.png";
         break;
     case 6:
-        document.querySelector("img.img2").src="images/dice6.png";
+        document.querySelector("img.img2").src="dice6.png";
         break;
 
 }
